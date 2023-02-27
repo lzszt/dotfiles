648b73127a0f294a5666e167a88ad581355d2066
Felix Leitz <felix.leitz92@gmail.com>
2023-02-27 21:24:13 +0100

    Enable virtualization.

--- configuration.nix
+++ configuration.nix
@@ -57,12 +57,16 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.leitz = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    extraGroups =
+      [ "wheel" "networkmanager" "docker" ]; # Enable ‘sudo’ for the user.
   };
   users.users.ag = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    extraGroups =
+      [ "wheel" "networkmanager" "docker" ]; # Enable ‘sudo’ for the user.
   };
+
+  virtualisation.docker.enable = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget

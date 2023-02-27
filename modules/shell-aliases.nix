8c03f7fa39a99bc11fe9fe63a39992a64e0da9fc
Felix Leitz <felix.leitz92@gmail.com>
2023-02-27 21:39:56 +0100

    Adds aliases for direnv.

--- modules/shell-aliases.nix
+++ modules/shell-aliases.nix
@@ -4,6 +4,11 @@
   # nixos
   nrs = "sudo nixos-rebuild switch --flake ~/dotfiles/";
 
+  # direnv
+  dea = "direnv allow";
+  ded = "direnv deny";
+  der = "direnv reload";
+
   # misc
   ll = "ls -lisah";
 }

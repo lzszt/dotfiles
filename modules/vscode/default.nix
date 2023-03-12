68fb25029cc9eed2e089d3ffb93d08172f28c61a
Felix Leitz <felix.leitz92@gmail.com>
2023-03-12 17:16:24 +0100

    Adds possibility for defining user and global vscode snippets.

--- modules/vscode/default.nix
+++ modules/vscode/default.nix
@@ -1,5 +1,48 @@
 { inputs, config, lib, pkgs, ... }:
-let cfg = config.modules.vscode;
+let
+  cfg = config.modules.vscode;
+
+  userSnippets = {
+    global = {
+      fixme = {
+        prefix = [ "fixme" ];
+        body = [ "$LINE_COMMENT FIXME (felix): $0" ];
+        description = "Insert a FIXME remark";
+      };
+    };
+    haskell = {
+      newtype = {
+        prefix = [ "newtype" ];
+        body = [ "newtype \${1:Foo} = $1 { \${2:un$1} :: $0}" ];
+        description = "Insert a newtype definition";
+      };
+      record = {
+        prefix = [ "record" ];
+        body = [
+          "data \${1:Foo} = $1 {"
+          "	\${1/(.)(.*)/\${1:/downcase}\${2:/camelcase}/}$2 :: $3,"
+          "	\${1/(.)(.*)/\${1:/downcase}\${2:/camelcase}/}$4 :: $0"
+          "	}"
+        ];
+        description = "Insert a data record defintion";
+      };
+      sumtype = {
+        prefix = [ "sumtype" ];
+        body = [ "data \${1:Foo}" "	= \${2:Bar}" "	| \${0:Baz}" ];
+        description = "Insert a data record defintion";
+      };
+      impl = {
+        prefix = [ "impl" ];
+        body = [ ''(error "implement ''${0:this}")'' ];
+        description = "Insert an implementation as error";
+      };
+      def = {
+        prefix = [ "def" ];
+        body = [ "$1 :: $2" ''$1 = ''${0:error "implement $1"}'' ];
+        description = "Generate a definition";
+      };
+    };
+  };
 in {
   options.modules.vscode = { enable = lib.mkEnableOption "vscode"; };
 
@@ -37,5 +80,20 @@ in {
         ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace
         (import ./extensions.nix).extensions;
     };
+    home.activation.vscode-userSnippets = let
+      snippetDir = "$HOME/.config/Code/User/snippets";
+      generateLanguageSnippetsFile = language: snippets:
+        if language == "global" then ''
+          SNIPPET_FILE_CONTENT='${builtins.toJSON snippets}'
+          echo $SNIPPET_FILE_CONTENT | ${pkgs.jq}/bin/jq . > ${snippetDir}/global.code-snippets
+        '' else ''
+          SNIPPET_FILE_CONTENT='${builtins.toJSON snippets}'
+          echo $SNIPPET_FILE_CONTENT | ${pkgs.jq}/bin/jq . > ${snippetDir}/${language}.json
+        '';
+
+      script = lib.concatLines ([ "mkdir -p ${snippetDir}" ]
+        ++ lib.mapAttrsToList generateLanguageSnippetsFile userSnippets);
+    in lib.hm.dag.entryAfter [ "writeBoundary" ] script;
   };
+
 }

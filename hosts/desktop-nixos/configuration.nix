94327ef1636e80ba9a6c96e57ed6b3998af104fb
Felix Leitz <felix.leitz92@gmail.com>
2023-06-02 19:30:05 +0200

    Zap comment.

--- hosts/desktop-nixos/configuration.nix
+++ hosts/desktop-nixos/configuration.nix
@@ -1,10 +1,7 @@
 { config, pkgs, lib, custom, ... }:
 
 {
-  imports = [ # Include the results of the hardware scan.
-    ./hardware-configuration.nix
-    ./../../wm/xmonad.nix
-  ];
+  imports = [ ./hardware-configuration.nix ../../wm/xmonad.nix ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;

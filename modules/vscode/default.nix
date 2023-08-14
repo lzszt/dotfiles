d23e0a849cd876a1be47ebdbcb8dea72a2e57fb3
Felix Leitz <felix.leitz@active-group.de>
2023-08-14 09:42:33 +0200

    vscode: Extract user settings into seperate file

--- modules/vscode/default.nix
+++ modules/vscode/default.nix
@@ -2,39 +2,7 @@
 let
   cfg = config.modules.vscode;
 
-  userSettings = {
-    # editor settings
-    "editor.minimap.enabled" = false;
-    "editor.tabSize" = 2;
-    "editor.formatOnSave" = true;
-    # telemetry settings
-    "telemetry.telemetryLevel" = "off";
-    # miscellaneous settings
-    "workbench.colorThem" = "Default Dark+";
-    "window.zoomLevel" = 1;
-    # git settings
-    "git.confirmSync" = false;
-    "git.autofetch" = true;
-    "git.autoStash" = true;
-
-    # search settings
-    "search.collapseResults" = "auto";
-    "search.useGlobalIgnoreFiles" = true;
-
-    # explorer settings
-    "explorer.confirmDragAndDrop" = false;
-
-    # default haskell settings
-    "haskell.formattingProvider" = "ormolu";
-    "haskell.manageHLS" = "PATH";
-
-    # stl viewer settings
-    "stlViewer.showAxes" = true;
-    "stlViewer.showInfo" = true;
-    "stlViewer.showBoundingBox" = true;
-    "stlViewer.meshMaterialType" = "normal";
-    "stlViewer.viewOffset" = 100;
-  };
+  userSettings = import ./user-settings.nix;
 
   snippets = import ./snippets.nix;
   extensions = import ./extensions.nix { inherit inputs system pkgs; };
---
+++ modules/vscode/user-settings.nix
@@ -0,0 +1,33 @@
+{
+  # editor settings
+  "editor.minimap.enabled" = false;
+  "editor.tabSize" = 2;
+  "editor.formatOnSave" = true;
+  # telemetry settings
+  "telemetry.telemetryLevel" = "off";
+  # miscellaneous settings
+  "workbench.colorThem" = "Default Dark+";
+  "window.zoomLevel" = 1;
+  # git settings
+  "git.confirmSync" = false;
+  "git.autofetch" = true;
+  "git.autoStash" = true;
+
+  # search settings
+  "search.collapseResults" = "auto";
+  "search.useGlobalIgnoreFiles" = true;
+
+  # explorer settings
+  "explorer.confirmDragAndDrop" = false;
+
+  # default haskell settings
+  "haskell.formattingProvider" = "ormolu";
+  "haskell.manageHLS" = "PATH";
+
+  # stl viewer settings
+  "stlViewer.showAxes" = true;
+  "stlViewer.showInfo" = true;
+  "stlViewer.showBoundingBox" = true;
+  "stlViewer.meshMaterialType" = "normal";
+  "stlViewer.viewOffset" = 100;
+}

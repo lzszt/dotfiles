023e9929aa7ec265f56ced253efff5d71e382cef
Felix Leitz <felix.leitz@active-group.de>
2023-08-22 08:10:30 +0200

    work: Fix typo.

--- users/work/home.nix
+++ users/work/home.nix
@@ -66,7 +66,7 @@ in {
           (mkWorkspace "remote" [ ])
           (mkWorkspace "5" [ "keepassxc" ])
           (mkWorkspace "cal" [ "thunderbird" ])
-          (mkWorkspace "com" [ "jitis-meet-electron" "linphone" ])
+          (mkWorkspace "com" [ "jitsi-meet-electron" "linphone" ])
           (mkWorkspace "8" [ ])
           (mkWorkspace "9" [ ])
         ];

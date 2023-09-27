33a2eb4f4fe8a3f8595bd118d6d9be3cbc891814
Felix Leitz <felix.leitz@active-group.de>
2023-09-27 10:08:37 +0200

    leitz: Disable hid sensor hub/enable brightness control keys.

--- hosts/leitz/configuration.nix
+++ hosts/leitz/configuration.nix
@@ -4,9 +4,12 @@
   imports = [ ./hardware-configuration.nix ../../wm/xmonad.nix ];
 
   # Use the systemd-boot EFI boot loader.
-  boot.loader = {
-    systemd-boot.enable = true;
-    efi.canTouchEfiVariables = true;
+  boot = {
+    extraModprobeConfig = "blacklist hid_sensor_hub";
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
   };
 
   networking = {

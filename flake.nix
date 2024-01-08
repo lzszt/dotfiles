ed12576f6b27f368dfd43c7f7d38907affb23b27
Felix Leitz <felix.leitz@active-group.de>
2024-01-08 18:40:00 +0100

    Update nixpkgs to PR commit to fix mattermost SSO not working.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url =
+      "github:NixOS/nixpkgs/36f57efd82062a44f420d97ddb02b0bf3853ecfe";
+    # nixpkgs.url = "git+file:/home/leitz/nixpkgs";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -30,7 +32,7 @@
 
           # Needed as long as obsidian still uses obsolete electron
           permittedInsecurePackages =
-            lib.optional (pkgs.obsidian.version == "1.4.16") "electron-25.9.0";
+            lib.optional (pkgs.obsidian.version == "1.5.3") "electron-25.9.0";
         };
         inherit overlays system;
       };

1b1b77cd51d3dc196c226bc43bc28640ab6ce2bd
Felix Leitz <felix.leitz92@gmail.com>
2024-05-17 09:47:22 +0200

    vscode: Remove editor-layout-extension.

--- modules/vscode/extensions.nix
+++ modules/vscode/extensions.nix
@@ -51,12 +51,6 @@ with pkgs.vscode-extensions;
     ]
     ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
       {
-        publisher = "ctf0";
-        name = "save-editors-layout";
-        version = "1.0.1";
-        sha256 = "sha256-8YnAP+njpuuZZkq9YokZP6e4H43jMHPQKHbJSLhbc5w=";
-      }
-      {
         publisher = "Kelvin";
         name = "vscode-sshfs";
         version = "1.26.1";

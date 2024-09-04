d179c9ca86a2bdccae49eb7c8e02c963208eed61
Felix Leitz <felix.leitz@active-group.de>
2024-09-04 11:32:57 +0200

    vscode: Fix some keybindings for agda-mode

--- modules/vscode/extensions/agda-mode.nix
+++ modules/vscode/extensions/agda-mode.nix
@@ -1,4 +1,21 @@
 { pkgs, ... }:
 {
   extension = pkgs.vscode-extensions.banacorn.agda-mode;
+  keybindings = [
+    {
+      key = "ctrl+x ctrl+=";
+      command = "-agda-mode.lookup-symbol";
+      when = "!terminalFocus && editorLangId == 'agda' || !terminalFocus && editorLangId == 'lagda-md' || !terminalFocus && editorLangId == 'lagda-rst' || !terminalFocus && editorLangId == 'lagda-tex'";
+    }
+    {
+      key = "ctrl+j ctrl+h";
+      command = "agda-mode.helper-function-type[Normalised]";
+      when = "editorLangId == 'agda' || editorLangId == 'lagda-md' || editorLangId == 'lagda-rst' || editorLangId == 'lagda-tex'";
+    }
+    {
+      key = "ctrl+y ctrl+h";
+      command = "-agda-mode.helper-function-type[Normalised]";
+      when = "editorLangId == 'agda' || editorLangId == 'lagda-md' || editorLangId == 'lagda-rst' || editorLangId == 'lagda-tex'";
+    }
+  ];
 }

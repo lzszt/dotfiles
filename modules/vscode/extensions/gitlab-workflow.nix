ff601fc5dbb8a1bd01e7363c15a67d2f7224dc7f
Felix Leitz <felix.leitz@active-group.de>
2024-10-01 07:28:35 +0200

    vscode: Add gitlab extension

---
+++ modules/vscode/extensions/gitlab-workflow.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }:
+{
+  extension = pkgs.vscode-extensions.gitlab.gitlab-workflow;
+  user-settings.gitlab = {
+    aiAssistedCodeSuggestions.enabled = false;
+    duo.enabledWithoutGitlabProject = false;
+    duoChat.enabled = false;
+    duoCodeSuggestions.enabled = false;
+    showPipelineUpdateNotifications = true;
+  };
+  default = false;
+}

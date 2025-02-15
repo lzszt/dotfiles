a18b7c42c8f5ffa43225bf70ad2c4eb23b9a3277
Felix Leitz <felix.leitz92@gmail.com>
2025-02-15 13:41:14 +0100

    firefox: Add extensions via NUR

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -1,13 +1,24 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  inputs,
+  pkgs,
+  ...
+}:
 let
   cfg = config.modules.firefox;
+  ffad = pkgs.nur.repos.rycee.firefox-addons;
 in
 {
   options.modules.firefox.enable = lib.mkEnableOption "firefox";
   config = lib.mkIf cfg.enable {
     programs.firefox = {
       enable = true;
-      profiles.myprofile.extensions = [ ];
+      profiles.myprofile.extensions = [
+        ffad.keepa
+        ffad.privacy-badger
+        ffad.keepassxc-browser
+      ];
     };
   };
 }

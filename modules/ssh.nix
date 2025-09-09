d2fa69139f77dabf7375b8c64d8bb1023cf9d7f5
Felix Leitz <felix.leitz@active-group.de>
2025-09-09 13:32:38 +0200

    ssh: Make old default config explicit

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -13,7 +13,20 @@ in
     programs.ssh = {
       enable = true;
 
-      matchBlocks = cfg.matchBlocks;
+      matchBlocks = cfg.matchBlocks // {
+        "*" = {
+          forwardAgent = false;
+          addKeysToAgent = "no";
+          compression = false;
+          serverAliveInterval = 0;
+          serverAliveCountMax = 3;
+          hashKnownHosts = false;
+          userKnownHostsFile = "~/.ssh/known_hosts";
+          controlMaster = "no";
+          controlPath = "~/.ssh/master-%r@%n:%p";
+          controlPersist = "no";
+        };
+      };
     };
   };
 }

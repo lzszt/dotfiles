f9efda68c2c53089973e48b64da81adbdf657912
Felix Leitz <felix.leitz@active-group.de>
2026-03-11 06:17:56 +0100

    work: Add new ssh public key for secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   desktop-nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGLX/ID/W4Q57WLqXSvYD66v8mq9PTBW8J1KCsXNDGj8 leitz@desktop-nixos";
-  leitz = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG6o4SiFpsgaR5H7MH2slofck0Mn8rveACBMPqKi/N4k leitz@leitz";
+  leitz = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEImW73Lb0y0UfZJ/+Pyhcxby9WpXppkCUuE+EFwVRd2 leitz@leitz";
   netbook = "";
   all = [
     desktop-nixos

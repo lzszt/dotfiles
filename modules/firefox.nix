8bd7905da5b64c7c80fa0eef91f176093810cc8f
Felix Leitz <felix.leitz@active-group.de>
2026-04-27 09:52:40 +0200

    Update

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -13,6 +13,8 @@ in
   config = lib.mkIf cfg.enable {
     programs.firefox = {
       enable = true;
+      # You are currently using the legacy default (`".mozilla/firefox"`) because `home.stateVersion` is less than "26.05".
+      configPath = ".mozilla/firefox";
       profiles.myprofile.extensions.packages = [
         ffad.adblocker-ultimate
         ffad.cookie-autodelete

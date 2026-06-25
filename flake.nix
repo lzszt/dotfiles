8e7d0aa7424a739f399c5051ff0e48163191a36b
Felix Leitz <felix.leitz92@gmail.com>
2026-06-25 18:52:30 +0200

    flake: Add nixpkgs master as input
    Fix typo
    
    fix

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-master.url = "github:NixOS/nixpkgs/master";
 
     home-manager = {
       url = "github:nix-community/home-manager";
